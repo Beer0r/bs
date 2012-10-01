a432829e97300e5d50159420e11b6428127ca9b3
Simon <beer0r@gmail.com>
2012-10-01 19:48:56 +0200

    Update CallByVal

--- CallByValue/test.c
+++ CallByValue/test.c
@@ -2,12 +2,12 @@
 
 int main() {
  int a = 10;
- printf("a: %d\n", a);
+ printf("a1: %d\n", a);
  sub(a);
+ printf("a2: %d\n", a);
 }
 
-sub(int a) {
- int b;  
- b = a - 1;
- printf("b: %d\n", b);
+sub(int a) { 
+ a = a - 1;
+ printf("a-sub: %d\n", a);
 }
